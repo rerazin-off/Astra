dd58467a161718532c4736ca9aa3c6f8204a5f02
rerazin <soldatkinaanastasia2007.24@gmail.com>
2026-04-23 01:23:54 +0300

    chaeck

--- Astra/Astra/urls.py
+++ Astra/Astra/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('module_project.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
