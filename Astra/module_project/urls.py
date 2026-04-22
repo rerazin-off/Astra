823c4d2ad5200fee88d847a8b17098e8ce53ac2a
rerazin <soldatkinaanastasia2007.24@gmail.com>
2026-04-22 23:22:56 +0300

    readme add images

--- Astra/module_project/urls.py
+++ Astra/module_project/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'module_project'
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
